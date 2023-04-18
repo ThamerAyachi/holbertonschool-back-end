d40d83647b5f14ad0d9ae7dfffffb3288b25d26e
ThamerAyachi <thamerjak3@gmail.com>
2023-04-18 16:42:58 +0100

    set task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Create an API to get todo list for employe"""
+
+import requests
+from sys import argv
+
+url_base = "https://jsonplaceholder.typicode.com/users/"
+
+
+if __name__ == "__main__":
+    employee = requests.get(url_base + argv[1]).json()
+    todo_list = requests.get(url_base + argv[1] + "/todos/").json()
+    completed_tasks = 0
+    text = ""
+
+    for task in todo_list:
+        if task['completed'] is True:
+            text += "\t {}\n".format(task['title'])
+            completed_tasks += 1
+
+    print("Employe {} is done with tasks({}/20):\n{}"
+          .format(employee['name'],
+                  completed_tasks,
+                  text), end="")
